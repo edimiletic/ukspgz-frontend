9b5c6715b9a6ea87fdd2c104d68e16b51ebc262a
Edi Miletic <edi.miletic1@gmail.com>
2025-08-29 16:56:59 +0200

    deployment config

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,70 @@
+// src/app/app.routes.server.ts
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  // Static routes that can be prerendered
+  {
+    path: '',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'home',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'login',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'basket-rules',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'statistics',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'games-assigned',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'expenses',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'time-absent',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'exams',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'notifications',
+    renderMode: RenderMode.Prerender
+  },
+  
+  // Dynamic routes that need server-side rendering
+  {
+    path: 'expenses/**',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'exams/take/**',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'exams/review/**',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'exams/result/**',
+    renderMode: RenderMode.Server
+  },
+  
+  // Catch-all for any other routes
   {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Server
   }
 ];
